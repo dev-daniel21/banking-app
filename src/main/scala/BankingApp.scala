110a124a13860ff1f276dfa54840542cb1da3030
dev-daniel21 <dev.daniel21@gmail.com>
2024-08-24 20:34:12 +0200

    customer added

--- src/main/scala/BankingApp.scala
+++ src/main/scala/BankingApp.scala
@@ -13,22 +13,33 @@ object BankingApp {
     val allBankProducts = Set(basicCheckingAccount, studentsCheckingAccount, basicSavingsAccount, creditCardAccount)
 
     val customerMichael = new Customer("Michael", "Scott", Email("mscott", "dunder.com"), LocalDate.of(1962, 2, 23))
-    val allCustomers = Set(customerMichael)
+    val customerJim = new Customer("Jim", "Halpert", Email("jhalpert", "dunder.com"), LocalDate.of(1972, 5, 16))
+    val allCustomers = Set(customerMichael, customerJim)
 
     val michaelBasicAcc = new DepositAccount(customerMichael, basicCheckingAccount, Dollars(3234))
-    val michaelsavingsAcc = new DepositAccount(customerMichael, basicSavingsAccount, Dollars(654))
+    val michaelSavingsAcc = new DepositAccount(customerMichael, basicSavingsAccount, Dollars(654))
     val michaelCreditAcc = new LendingsAccount(customerMichael, creditCardAccount, Dollars(1732))
 
-    val michaelAllAccounts = Set(michaelBasicAcc, michaelsavingsAcc, michaelCreditAcc)
+    val jimBasicAcc = new DepositAccount(customerJim, basicCheckingAccount, Dollars(1753))
+    val jimCreditAcc = new LendingsAccount(customerJim, creditCardAccount, Dollars(437))
 
-    val bank = new Bank("New Corporate Bank", "Scranton", "USA", allBankProducts, allCustomers, michaelAllAccounts)
+    val customersAccounts = Set(michaelBasicAcc, michaelSavingsAcc, michaelCreditAcc, jimBasicAcc, jimCreditAcc)
+
+    val bank = new Bank("New Corporate Bank", "Scranton", "USA", allBankProducts, allCustomers, customersAccounts)
 
     println(michaelBasicAcc)
     michaelBasicAcc.makeDeposit(43)
     println(michaelBasicAcc.getBalance)
     michaelBasicAcc.makeWithdrawal(32)
     println(michaelBasicAcc)
-
+    println(michaelSavingsAcc)
+    println(michaelCreditAcc)
+    println(jimBasicAcc.toString)
+    println(jimCreditAcc.toString)
+    jimCreditAcc.lendMoney(54)
+    println(jimCreditAcc.getBalance)
+    jimCreditAcc.makePayments(96)
+    print(jimCreditAcc)
   }
 
 }
